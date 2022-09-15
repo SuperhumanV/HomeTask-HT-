1f6e4b3e145e31970ad94fd57894608c71ba50a5
Vad <poriata@yandex.ru>
2022-09-15 14:57:50 +0300

    3num&max

---
+++ task#4/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine ("Input number 1");
+string? lineA = Console.ReadLine ();
+
+Console.WriteLine ("Input number 2");
+string? lineB = Console.ReadLine ();
+
+Console.WriteLine ("Input number 3");
+string? lineC = Console.ReadLine();
+if(lineA !=null&& lineB !=null&& lineC !=null)
+{
+    int numA = Int32.Parse(lineA);
+    int numB = Int32.Parse(lineB);
+    int numC = Int32.Parse(lineC);
+    int max = numA;
+    int max2 = 0;
+    if(numB>numC)
+    {
+        max2=numB;
+    }
+    else
+    {
+        max2=numC;
+    }
+   
+    if(max>max2)
+    {
+        Console.WriteLine("max =");
+        Console.WriteLine(max);
+    }
+    
+    else
+    {
+        Console.WriteLine("max =");
+        Console.WriteLine(max2);
+    }
+}
