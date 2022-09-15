da0bcbb4518e427cb55958001c7977da7746fa33
Vad <poriata@yandex.ru>
2022-09-15 13:44:11 +0300

    task2homework

---
+++ task#2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Input number 1");
+int numA =Int32.Parse(Console.ReadLine ());
+Console.WriteLine ("Input number 2");
+int numB = Int32.Parse(Console.ReadLine ());
+if(numA>numB)
+{
+    Console.WriteLine("max=");
+    Console.WriteLine(numA);
+}
+else 
+{
+    Console.WriteLine("max =");
+    Console.Write(numB);
+}
