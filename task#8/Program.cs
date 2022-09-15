ada91c790f9241a858a709f6790845deb7424969
Vad <poriata@yandex.ru>
2022-09-15 15:29:51 +0300

    even numbers

---
+++ task#8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Input your number");
+string? line1 = Console.ReadLine();
+
+if(line1 != null)
+{
+    int num = Int32.Parse(line1);
+    int i = 1;
+   Console.WriteLine("Even numbers");
+    while(i<=num)
+    { 
+        if(i%2==0)
+        {
+            Console.WriteLine(i);
+        }
+        i++;
+
+    }
+}
