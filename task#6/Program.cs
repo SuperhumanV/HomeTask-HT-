d43f59d8f7313b81c42cb064f0bee6d1ab427be5
Vad <poriata@yandex.ru>
2022-09-15 14:55:04 +0300

    Even&Odd

---
+++ task#6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input your number");
+string? lineA = Console.ReadLine();
+
+if(lineA != null)
+{
+   int num1 = Int32.Parse(lineA);
+    if(num1%2==0)
+    {
+        Console.WriteLine("Even");
+    }
+    else
+    {
+        Console.WriteLine("Odd");
+    }
+}
